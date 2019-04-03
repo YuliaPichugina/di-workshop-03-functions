c9297d8eeda4adbd0944537ac6cd02e1522f6ed7
YuliaPichugina <yulia.pichugina@ada.ac.uk>
2019-04-03 11:50:38 +0100

    added square function

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function setup() {
+function setup() { // function creates a canvas with set up size and colour
     createCanvas(400, 400)
     background(255)
   }
@@ -6,6 +6,10 @@ function setup() {
   function draw() {
     fill(255, 130, 0)
     stroke(0)
-    triangle(0, 30, 40, 0, 80, 30)
+    triangle(0, 30, 40, 0, 80, 30) 
+    square(5, 30, 70)
+  }
+
+  function square(5, 30, 70) {
     rect(5, 30, 70, 70)
   }
